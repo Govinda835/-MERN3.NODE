b50007c7384bedb57e1a229c35e9a171da8f9cc0
Govinda835 <chaudharygovinda835@gmail.com>
2024-12-31 22:26:17 +0545

    first commit

--- app.js
+++ app.js
@@ -17,11 +17,3 @@ app.get("/about",(req,res)=>{
 app.listen(3000,()=>{
     console.log("Node.Js project has been started!!!");
 })
-
-echo "# -MERN3.NODE" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Govinda835/-MERN3.NODE.git
-git push -u origin main
